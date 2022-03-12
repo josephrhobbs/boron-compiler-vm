d2d479d60176d3e291214c422740b800e96d7daf
hobbsbros <89106819+hobbsbros@users.noreply.github.com>
2022-03-12 09:44:21 -0500

    Fixed VM filesystem bug
    Repaired bug preventing VM from accessing files when executed

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ mod util;
 
 use vm::memory;
 use util::config;
-use std::fs;
 
 fn main() {
     let configuration: config::Config = config::configure();
     println!("{:?}", configuration.program);
 
     let mut virtual_machine = memory::initialize_vm();
-    virtual_machine.load_program(vec![1u8; 100]);
+    virtual_machine.load_program(configuration.program);
 }
--- src/util/config.rs
+++ src/util/config.rs
@@ -2,6 +2,7 @@
 // Configures files on start-up
 
 use std::env;
+use std::io::Read;
 use std::fs;
 
 pub struct Config {
@@ -10,7 +11,16 @@ pub struct Config {
 
 pub fn configure() -> Config {
     let args: Vec<String> = env::args().collect();
-    let name: String = args[0].clone();
-    let p = fs::read(name).unwrap();
-    Config {program: p}
+    let name: String = args[1].clone();
+
+    // file must be mutable here as it is being read later in the program
+    // We also pass &name rather than name to prevent changing the parent scope of name
+    let mut file = fs::File::open(&name).expect("Could not find filename");
+    let metadata = fs::metadata(&name).expect("Unable to read metadata");
+
+    let mut buffer = vec![0u8; metadata.len() as usize];
+    // We pass &mut buffer to prevent changing the scope of buffer while still permitting modification
+    file.read(&mut buffer).expect("Buffer overflow");
+
+    Config {program: buffer}
 }
